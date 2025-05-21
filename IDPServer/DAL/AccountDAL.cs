49718941535300ee251255f516376941ac8d946c
Erick Kurniawan <erick.kurniawan@gmail.com>
2025-05-21 10:10:13 +0700

    implementasi asp identity

---
+++ IDPServer/DAL/AccountDAL.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace IDPServer.DAL
+{
+    public class AccountDAL : IAccountIDP
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        public AccountDAL(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public Task AddRole(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task AddRolesToUser(string username, List<string> roleNames)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task AddUserToRole(string username, string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteRole(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IdentityRole> GetRole(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<string>> GetRolesFromUser(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IdentityUser> GetUser(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IdentityUser> GetUserById(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Login(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IdentityUser> Register(IdentityUser user, string password)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- IDPServer/Program.cs
+++ IDPServer/Program.cs
@@ -1,8 +1,29 @@
+using IDPServer.DAL;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(builder =>
+{
+    builder.Password.RequiredLength = 6;
+    builder.Password.RequireDigit = true;
+    builder.Password.RequireLowercase = true;
+    builder.Password.RequireUppercase = true;
+    builder.Password.RequireNonAlphanumeric = true;
+    builder.User.RequireUniqueEmail = true;
+})
+    .AddEntityFrameworkStores<AppDbContext>()
+    .AddDefaultTokenProviders();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
